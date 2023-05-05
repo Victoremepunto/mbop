8a22d9637835615f65b3be1aed228dbe43a158a3
Keith Walsh <kwalsh@redhat.com>
2023-05-05 07:05:11 -0400

    Adds support for Keycloak JWT module
    Since Keycloak and Cognito follow the JWK standard [1], we can reuse the same
    logic as defined in the `awsModule`. For the sake of being explicit, this adds
    another module `keycloakModule`, so that in the future if any module-specific
    needs arise, we can easily break them out, and it will also make it clear which
    JWK/JWT verification backing is used in an environment.
    
    [1] https://www.ietf.org/rfc/rfc7517.txt

--- internal/handlers/constants.go
+++ internal/handlers/constants.go
@@ -8,6 +8,7 @@ const awsModule = "aws"
 const amsModule = "ams"
 const mockModule = "mock"
 const printModule = "print"
+const keycloakModule = "keycloak"
 
 const defaultLimit = 100
 const defaultOffset = 0
